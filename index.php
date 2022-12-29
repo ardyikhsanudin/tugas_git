63be506cf6541c966d37000cd30d0afc1ef37f7b
M. Ardy Ikhsanudin <121593174+ardyikhsanudin@users.noreply.github.com>
2022-12-29 11:33:13 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+ <head>
+    <title>Home</title>
+    <!-- Created By : M. Ardy Ikhsanudin  -->
+</head>
+<body>
+    <?php
+    echo "<h2> PHP Array </h2>";
+    $food = array("Sate", "Bakso", "Ayam Goreng");
+    echo "Favorit makanan saya adalah " . $food[0] . ", " . $food[1] . " and " . $food[2] . ".";
+    ?>
+</body>
+ 
+</html>
